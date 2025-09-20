b1bc7886d07def5cffce70bb07147c0c69aa6c2f
leovit <leonardogondor@gmail.com>
2025-09-20 06:39:07 -0300

    calcuradora totalmente funcional

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-function inserirOperador(sinal) {
-    const input = document.getElementById('display');
-    input.value += sinal;
+function inserirOperador(value) {
+    document.getElementById('display').value += value;
+}
+
+const limpaDisplay = () => document.getElementById('display'). value = '';
+
+function calcular() {
+    try {
+        const resultado = eval(document.getElementById('display').value);
+        document.getElementById('display').value = resultado;
+    } catch {
+        document.getElementById('display').value = 'Erro';
+    }
 }
